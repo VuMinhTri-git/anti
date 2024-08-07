1972a643e33ba114d067f38cab929caaf38d10c6
VuMinhTri-git <trivu3718@gmail.com>
2024-08-08 03:00:12 +0700

    Update auto-rule.sh

--- auto-rule.sh
+++ auto-rule.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 while true
 do
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/anti.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/VuMinhTri-git/anti/main/anti.sh)
 sleep 2
 done
