c5dade7dac2d4efa575cb63dc6e793365d82d12c
VuMinhTri-git <trivu3718@gmail.com>
2024-08-08 02:54:04 +0700

    Update Captcha-on.sh

--- Captcha-on.sh
+++ Captcha-on.sh
@@ -1,6 +1,6 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules/adb451c64204498d986435b508aa8a0e" \
-     -H "X-Auth-Email: aulax432@gmail.com" \
-     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules/45ed8ab9d25f1e87472043691373686f" \
+     -H "X-Auth-Email: trivu3718@gmail.com" \
+     -H "X-Auth-Key: b658158ea2cdbd6fd1eb323dd1c9c37c01db1" \
      -H "Content-Type: application/json" \
      --data '{
      "action": "managed_challenge",
