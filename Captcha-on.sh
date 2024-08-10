f729c27bb12b97803718a347d66416ee80dfc888
VuMinhTri-git <trivu3718@gmail.com>
2024-08-11 00:10:25 +0700

    Update Captcha-on.sh

--- Captcha-on.sh
+++ Captcha-on.sh
@@ -6,11 +6,11 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/45ed8ab9d25f1e8747204369
      "action": "managed_challenge",
      "priority": 2000,
      "paused": false,
-     "description": "Captcha việt",
+     "description": "Managed Challenge Việt Nam",
      "filter": {
     "id": "9bf149ab9ece4715b0fe1cfa0e36dfec",
-    "expression": "(ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS \" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Windows NT\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (not http.user_agent contains \"Zalo\") or (not http.request.uri.path in {\"/api/v1/guest/telegram/webhook\"}) or (not http.request.uri.path in {\"/api/v1/guest/payment/notify\"}) or (not http.user_agent contains \"go-resty/2.7.0 (https://github.com/go-resty/resty)\") or (http.request.uri.path contains \"/api/v1/server/\" and not http.request.uri.query contains \"thaythanhapikey\")",
+    "expression": "(http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Windows NT\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"})",
     "paused": false,
-    "description": "Captcha việt"
+    "description": "Managed Challenge Việt Nam"
   }
   }'
